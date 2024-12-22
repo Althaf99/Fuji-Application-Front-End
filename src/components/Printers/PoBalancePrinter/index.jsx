729f869b0b8801727642746406fb95a5185b2e18
Althaf Azad <althafazad@Althafs-MacBook-Air.local>
2024-12-22 11:33:04 +0530

    Updated the data on stock

--- src/components/Printers/PoBalancePrinter/index.jsx
+++ src/components/Printers/PoBalancePrinter/index.jsx
@@ -33,10 +33,32 @@ const columns = [
   },
   {
     Header: "PO Balance",
-    accessor: "quantity",
+    accessor: "poBalanceQuantity",
     headerStyles: { textAlign: "center" },
     cellStyles: { textAlign: "center" },
     Cell: ({ value }) => <>{value.toLocaleString()}</>,
+    width: "25%",
+  },
+  {
+    Header: "Stock Balance",
+    accessor: "stockBalanceQuantity",
+    headerStyles: { textAlign: "center" },
+    cellStyles: { textAlign: "center" },
+    Cell: ({ value }) => <>{value.toLocaleString()}</>,
+    width: "25%",
+  },
+  {
+    Header: "Total Shots",
+    accessor: "totalShots",
+    headerStyles: { textAlign: "center" },
+    cellStyles: { textAlign: "center" },
+    width: "25%",
+  },
+  {
+    Header: "Total Hours",
+    accessor: "totalHours",
+    headerStyles: { textAlign: "center" },
+    cellStyles: { textAlign: "center" },
     width: "25%",
   },
 ];
@@ -44,46 +66,41 @@ const columns = [
 export const PoBalancePrinter = forwardRef((props, ref) => {
   const classes = styles();
 
-  const { data: report } = useReport({
-    itemName: "",
-    itemColor: "",
-    requestNumber: "",
-    date: "",
-  });
+  const { data: report } = useReport();
 
-  console.log("report",report)
-
-  const filteredRequestArray = report?.filter(
-    (element) => element.quantity > 0
-  );
-
-  filteredRequestArray?.sort((a, b) => a.itemName.localeCompare(b.itemName));
-
-  const resultArray = [];
-
-  filteredRequestArray?.forEach((item) => {
-    const existingItem = resultArray.find(
-      (i) => i.itemName === item.itemName && i.itemColor === item.itemColor
-    );
-
-    if (existingItem) {
-      existingItem.quantity += item.quantity;
-    } else {
-      resultArray.push({
-        no: item.no,
-        item: `${item.itemName} ${item.itemColor}`,
-        quantity: item.quantity,
-        unitPrice: item.unitPrice,
-        itemColor: item.itemColor,
-        itemName: item.itemName,
-      });
-    }
-  });
+  report?.sort((a, b) => a.itemName.localeCompare(b.itemName));
 
   let no = 0;
-  resultArray?.forEach((element) => {
+  report?.forEach((element) => {
     no = no + 1;
     element.no = no;
+    element.item = `${element.itemName} ${element.itemColor}`;
+  });
+
+  const updatedDataArray = report.map((obj) => {
+    const totalShots =
+      obj.poBalanceQuantity > obj.stockBalanceQuantity
+        ? (
+            (obj.poBalanceQuantity - obj.stockBalanceQuantity) /
+            obj.cavity
+          ).toFixed(0)
+        : 0;
+
+    const totalHours =
+      obj.poBalanceQuantity > obj.stockBalanceQuantity
+        ? (
+            ((obj.poBalanceQuantity - obj.stockBalanceQuantity) *
+              obj.cycleTime) /
+            (obj.cavity * 3600)
+          ).toFixed(0)
+        : 0;
+
+    // Return a new object with added properties
+    return {
+      ...obj, // Spread the original properties
+      totalHours,
+      totalShots,
+    };
   });
 
   const marginTop = "10px";
@@ -96,22 +113,19 @@ export const PoBalancePrinter = forwardRef((props, ref) => {
 
   const pageStyle = `
     @page {
-      size: A4;
-    }
-    @media all {
-      .page-break {
-        display: none;
-      }
-    }
-    @media print {
-      .page-break {
-        page-break-before: always;
-      }
+    size: A4 landscape;
+    margin: 0;
+  }
+  @media print {
+    .printable-container {
       width: 100%;
-      .header, .footer {
-        /* Your header and footer styles from styles.js here */
-      }
+      margin: 0 auto;
     }
+    table {
+      width: 90%;
+      border-collapse: collapse;
+    }
+  }
   `;
 
   return (
@@ -135,10 +149,10 @@ export const PoBalancePrinter = forwardRef((props, ref) => {
           </Grid>
         </thead>
         <tbody>
-          {resultArray && columns && (
+          {updatedDataArray && columns && (
             <PrintableTable
               columns={columns}
-              data={resultArray}
+              data={updatedDataArray}
               customProps={{ height: "600px" }}
               hiddenColumns={["id"]}
               fontSize="24px"
